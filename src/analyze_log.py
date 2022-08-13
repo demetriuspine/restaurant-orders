f04bb2a10ca751dcb380e2e6ea57630c5bad5539
lucas <mr.pine97@gmail.com>
2022-08-12 21:24:19 -0300

    feat: adiciona arnaldo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -6,5 +6,11 @@ def maria(name, order, maria_favorite):
             maria_favorite[order] = 1
 
 
+def arnaldo(name, order, burger_orders):
+    if name == 'arnaldo' and order == 'hamburguer':
+        burger_orders += 1
+    return burger_orders
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
