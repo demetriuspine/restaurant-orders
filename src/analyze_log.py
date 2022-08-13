69319d5145987648a8577a25bf228128560545e9
lucas <mr.pine97@gmail.com>
2022-08-12 21:25:37 -0300

    feat: implementa analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def maria(name, order, maria_favorite):
     if name == 'maria':
         if order in maria_favorite:
@@ -20,4 +23,32 @@ def joao(name, order, day, days, joao_visit, menu, joao_orders):
         joao_visit.add(day)
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if 'csv' in path_to_file:
+        burger_orders = 0
+        maria_favorite = dict()
+        joao_visit = set()
+        joao_orders = set()
+        days = set()
+        menu = set()
+
+        try:
+            with open(path_to_file) as file:
+                file = csv.reader(file, delimiter=",")
+                for name, order, day in file:
+                    maria(name, order, maria_favorite)
+                    joao(name, order, day, days, joao_visit, menu, joao_orders)
+                    burger_orders = arnaldo(name, order, burger_orders)
+
+            with open('data/mkt_campaign.txt', 'w') as file:
+                file.writelines([
+                    f"{max(maria_favorite, key=maria_favorite.get)}\n",
+                    f"{burger_orders}\n",
+                    f"{menu - joao_orders}\n",
+                    f"{days - joao_visit}",
+                ])
+
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    else:
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
