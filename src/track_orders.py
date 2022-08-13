3b2bd51e4c17b291bccd4099670700fb01e051c2
lucas <mr.pine97@gmail.com>
2022-08-12 21:48:23 -0300

    feat: implementa a classe TrackOrders com seus métodos

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,49 @@
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders_list = []
+
     def __len__(self):
-        pass
+        orders_list_len = len(self.orders_list)
+        return orders_list_len
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders_list.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        count = dict()
+        for name, order, day in self.orders_list:
+            if name == customer:
+                if order not in count:
+                    count[order] = 1
+
+                count[order] += 1
+
+        most_ordered_dish_per_customer = max(count, key=count.get)
+
+        return most_ordered_dish_per_customer
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        orders = {
+            order for name, order, day in self.orders_list if name == customer
+            }
+        menu = {
+            order for name, order, day in self.orders_list
+        }
+        never_ordered_per_customer = menu - orders
+        return never_ordered_per_customer
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        visited_days = {
+            day for name, order, day in self.orders_list if name == customer
+            }
+        days = {
+            day for name, order, day in self.orders_list
+        }
+
+        days_never_visited_per_customer = days - visited_days
+
+        return days_never_visited_per_customer
 
     def get_busiest_day(self):
         pass
