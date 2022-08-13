d3cce5327fae2c490ec31807697ed01616fdf46a
lucas <mr.pine97@gmail.com>
2022-08-12 21:23:54 -0300

    feat: adiciona maria

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,10 @@
+def maria(name, order, maria_favorite):
+    if name == 'maria':
+        if order in maria_favorite:
+            maria_favorite[order] += 1
+        else:
+            maria_favorite[order] = 1
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
